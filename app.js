3bf215984d69efe9fa498512b3749f9838de8125
Khoa Le <khoale@cpp.edu>
2020-09-30 23:06:17 -0700

    uibuild

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/users', usersRouter)
 app.use('/api/notes', notesRouter)
-app.use(middleware.redirectHttps())
+app.use(middleware.redirectHttps)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
