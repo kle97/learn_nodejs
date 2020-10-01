49e28ca1259c7768408e0e562ba7c34e157e47fa
Khoa Le <khoale@cpp.edu>
2020-09-30 23:12:45 -0700

    uibuild

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const usersRouter = require('./controllers/users')
 const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware')
+app.use(middleware.redirectHttps)
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -22,11 +23,10 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-app.use(middleware.redirectHttps)
+
 app.use(middleware.requestLogger)
 app.use('/api/users', usersRouter)
 app.use('/api/notes', notesRouter)
-
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
