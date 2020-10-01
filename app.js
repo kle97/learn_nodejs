89174db1c8edb8f15fcaf5dbd341a4af0e7c132d
Khoa Le <khoale@cpp.edu>
2020-09-30 23:08:40 -0700

    uibuild

--- app.js
+++ app.js
@@ -22,10 +22,11 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.redirectHttps)
 app.use(middleware.requestLogger)
 app.use('/api/users', usersRouter)
 app.use('/api/notes', notesRouter)
-app.use(middleware.redirectHttps)
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
