63479b60111da0903ea0385063ebb0d4152bf0c6
Khoa Le <khoale@cpp.edu>
2020-10-02 00:02:09 -0700

    uibuild

--- build/precache-manifest.fee827bbf839a0ef87784731ff012532.js
+++ build/precache-manifest.fee827bbf839a0ef87784731ff012532.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9dee209d6e82a5e5a41dac5077cfd199",
+    "revision": "e2faba042026eabd6e37ea1d26d95f29",
     "url": "/index.html"
   },
   {
-    "revision": "eddec84d5f2cf38725f5",
+    "revision": "7558679e0afaf0deab1c",
     "url": "/static/css/main.216d52b0.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.a9071d7a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "eddec84d5f2cf38725f5",
-    "url": "/static/js/main.5533d219.chunk.js"
+    "revision": "7558679e0afaf0deab1c",
+    "url": "/static/js/main.cc49defa.chunk.js"
   },
   {
     "revision": "a1acb3a5688dd80cc8fe",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d32a7095270a42501d8dc897342805ba.js"
+  "/precache-manifest.fee827bbf839a0ef87784731ff012532.js"
 );
 
 self.addEventListener('message', (event) => {
